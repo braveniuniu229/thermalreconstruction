083036a3ee618b742c9539585d82ea8ec94cbcef
braveniuniu229 <130730801+braveniuniu229@users.noreply.github.com>
2024-03-27 11:23:35 +0800

    maskeddataset

---
+++ dataset/maskeddataset.py
@@ -0,0 +1,78 @@
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import torch
+
+class MaskedDataset(Dataset):
+    def __init__(self, labels,mask_ratio, train=True, train_ratio=0.8):
+        """
+        Custom dataset initializer.
+        :param data: The data (e.g., 'T' from your dataset)
+        :param labels: The labels or outputs corresponding to the data (e.g., 'o')
+        :param train: Boolean flag indicating whether this is training data. Default is True.
+        :param train_ratio: Ratio of data to be used for training. Default is 0.8.
+        """
+
+
+
+        self.labels = labels
+        self.mask_ratio = mask_ratio
+        self.train = train
+        # x = np.linspace(8, 55, 4, dtype=int)
+        # y = np.linspace(8, 55, 4, dtype=int)
+        # xv, yv = np.meshgrid(x, y)
+        # #这里是选择的测点
+        # self.points = np.vstack([xv.ravel(), yv.ravel()]).T
+
+
+        # Determine split sizes
+        num_samples_per_class = labels.shape[1]
+        num_train = int(num_samples_per_class * train_ratio)
+        self.indices = np.arange(num_samples_per_class)
+
+        if self.train:
+            self.indices = self.indices[:num_train]
+        else:
+            self.indices = self.indices[num_train:]
+
+    def add_random_mask(self, label):
+        total_pixels = label.numel()
+        num_masked = int(total_pixels * self.mask_ratio)
+        mask_indices = np.random.choice(total_pixels, num_masked, replace=False)
+        flat_label = label.view(-1)  # Flatten the label to 1D for easy indexing
+        flat_label[mask_indices] = 0  # Assuming 0 is the masking value
+        return flat_label.view_as(label)  # Reshape back to original shape
+
+
+
+    def __len__(self):
+        return len(self.indices) * self.labels.shape[0]  # Total number of samples
+
+    def __getitem__(self, idx):
+        class_idx = idx // len(self.indices)
+        sample_idx_in_class = self.indices[idx % len(self.indices)]
+        sample_label = self.labels[class_idx, sample_idx_in_class]
+        sample_label = sample_label.reshape(64,64)
+        masked_label = self.add_random_mask(torch.from_numpy(sample_label))
+        # sample_data = sample_label[self.points[:,0],self.points[:,1]]
+        return  masked_label,sample_label
+dataorigin = np.load('/mnt/d/codespace/DATASETRBF/Heat_Types1000_source4_number100_normalized.npz')
+labels = dataorigin['T']
+
+dataset_train = MaskedDataset(labels, train=True, train_ratio=0.8)
+dataset_test = MaskedDataset(labels, train=False, train_ratio=0.8)
+
+if __name__ =="__main__":
+    trainloader = DataLoader(dataset_train,shuffle=True,batch_size=800)
+    for masked_label ,label in trainloader:
+        print(masked_label.shape,label.shape)
+        import matplotlib.pyplot as plt
+
+
+        fig, axis = plt.subplots(6, 6, figsize=(16, 16), dpi=200)
+        fig.tight_layout()
+        for i in range(18):
+            axis.ravel()[2*i].imshow(masked_label[i], vmin=-2, vmax=2, cmap='bwr')
+            axis.ravel()[2 * i+1].imshow(label[i], vmin=-2, vmax=2, cmap='bwr')
+            axis.ravel()[i].axis('off')
+        plt.show()
+        break
