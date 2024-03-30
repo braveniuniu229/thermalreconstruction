b6574857eb04645aae8ba72cf6e6cc736122a53e
braveniuniu229 <qfzhyhuang@gmail.com>
2024-03-30 16:49:03 +0800

    incontextunet

--- utils/normalized.py
+++ utils/normalized.py
@@ -6,14 +6,15 @@ import numpy as np
 
 
 def normalized():
-    path = '../data/Heat_Types10000_source4_number10fixed.npz'
+    path = '../data/Heat_Types10_source4_number30fixed.npz'
     data = np.load(path)  # 使用self.path来访问实例变量
     origindata = data['T']
+    source = data['S']
     mean = np.mean(origindata.reshape(-1))
     std_var = np.std(origindata.reshape(-1))  # 直接计算标准差
     normalized_data = (origindata - mean) / std_var
     mid_path = path.rsplit('.npz', 1)[0]  # 移除.npz
     new_path = mid_path + "_normalized.npz"
-    np.savez(new_path, T=normalized_data,m=mean,v=std_var)
+    np.savez(new_path, T=normalized_data,m=mean,v=std_var,S=source)
 if __name__ == '__main__':
     normalized()
